6b87f1d18298ea5247e869da7292a3db51a7ead6
Juana-W8 <juana.wilches@o365.unab.edu.co>
2022-10-22 18:57:07 -0500

    cambios

--- js/suma.js
+++ js/suma.js
@@ -21,15 +21,20 @@ function realizar_operaciones_matematicas(){
 
     var numero1 = document.getElementById("numero1").value;
     var numero2 = document.getElementById("numero2").value;
+    var datos_completos=true;
+
     if(numero1==''){
        document.getElementById ("numero1").setAttribute("class","form-control is-invalid");
        var parrafo1 = document.getElementById ("parrafo_validar_numero1");
        parrafo1.innerHTML="Debe ingresar número 1";
        parrafo1.setAttribute("class","text-danger");
+       var datos_completos=false;
+
     }else{
         document.getElementById ("numero1").setAttribute("class","form-control is-valid"); 
         var parrafo1 = document.getElementById ("parrafo_validar_numero1");
         parrafo1.innerHTML="";
+
     }
 
     if(numero2==''){
@@ -43,6 +48,51 @@ function realizar_operaciones_matematicas(){
         var parrafo2 = document.getElementById ("parrafo_validar_numero2");
         parrafo2.innerHTML="";
     }
- 
 
+    if ((numero1!='')&&(numero2!='')){
+
+        document.getElementById("btnSumar").setAttribute ("class", "btn btn-info boton-formulario");
+        document.getElementById("btnSumar").setAttribute ("disabled","");
+        document.getElementById("btnCancelar").setAttribute ("class", "btn btn-info boton-formulario");
+
+        multiplicar(numero1,numero2);
+        restar(numero1,numero2);
+        sumar(numero1, numero2);
+    }
+    
+
+    function multiplicar(numero1, numero2){
+
+
+        var multiplicar_valor=numero1*numero2;
+
+        var caja_multiplicar = document.getElementById("div_multiplicar");
+        caja_multiplicar.innerHTML="La multiplicación es " +multiplicar_valor;
+
+
+    }
+    function restar(numero1, numero2){
+        var restar_valor=numero1-numero2;
+        
+        var caja_restar = document.getElementById("div_restar");
+        caja_restar.innerHTML="La resta es " +restar_valor;
+    }
+    
+    function sumar(numero1, numero2){
+        var sumar_valor=parseInt(numero1)+parseInt(numero2);
+    
+        var caja_sumar = document.getElementById("div_sumar");
+        caja_sumar.innerHTML="La suma es " +sumar_valor;
+
+    }
 }
+/*Código para crear una etiqueta h1 dentro de un div del html*/
+var elemento_crear = document.createElement("h1");
+
+var texto_h1 = document.createTextNode("texto para el h1");
+elemento_crear.appendChild(texto_h1);
+elemento_crear.setAttribute("class", "text-success caja_materias");
+
+var buscar_caja_h1 = document.getElementById ("caja-crear-etiquetas");
+
+buscar_caja_h1.appendChild(elemento_crear);
